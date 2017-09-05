2d0d1e18e3de28b36be7cf6ad289e49f239a76bb
Andriy Herasymyuk <andriy@MacBook-Pro-Andriy.local>
2017-09-05 15:25:27 +0300

    Initial Commit

---
+++ TestSuperMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestSuperMenu
+//
+//  Created by Andriy Herasymyuk on 05.09.17.
+//  Copyright © 2017 AndriyHerasymyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
